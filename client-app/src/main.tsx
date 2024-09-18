3e999f8bf402a49e1cc05739e8ff469078a62ca1
faizanalim <faizan.23@hotmail.com>
2024-09-18 08:03:12 +0500

    Crud Operation on react app Without API

--- client-app/src/main.tsx
+++ client-app/src/main.tsx
@@ -1,8 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
+import App from './layout/App.tsx'
 import 'semantic-ui-css/semantic.min.css'
-import './index.css'
+import './layout/style.css'
 
 
 createRoot(document.getElementById('root')!).render(
